5a648448efc7e6a0aec000e8bddd0f9b3a7ecfa9
kawasaki@cl.ics.tut.ac.jp <kawasaki@sagitta.cl.ics.tut.ac.jp>
2014-10-07 17:29:04 +0900

    .gitignore_globalを追加

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -3,4 +3,5 @@ ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.dir_colors ~/.dir_colors
 ln -s ~/dotfiles/.emacs ~/.emacs
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore_global ~/.gitconfig_global
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
